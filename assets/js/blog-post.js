6cd8da0beb3fbed06310556a8161348792a5bbe4
Letian Wang <52314483+Letian-Wang@users.noreply.github.com>
2026-07-15 09:25:05 +0100

    Change input position to bottom and update theme attribute

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -221,8 +221,8 @@
       script.setAttribute('data-strict', '0');
       script.setAttribute('data-reactions-enabled', '1');
       script.setAttribute('data-emit-metadata', '0');
-      script.setAttribute('data-input-position', 'top');
-      script.setAttribute('data-theme', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
+            script.setAttribute('data-input-position', 'bottom');
+          script.setAttribute('data-theme', 'preferred_color_scheme');
       script.setAttribute('data-lang', 'en');
       script.setAttribute('crossorigin', 'anonymous');
       script.async = true;
